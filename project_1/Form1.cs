a3592efbd540c76a38b10c63291327488f67cffb
Tashirostl <stl.tashiro@gmail.com>
2016-11-29 00:56:33 +0900

    苗字　と　名前　を連結してラベルに表示

--- project_1/Form1.cs
+++ project_1/Form1.cs
@@ -16,5 +16,26 @@ namespace project_1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string L_name = textBox1.Text;
+            string F_name = textBox2.Text;
+
+            if (L_name.Length > 6)
+            {
+                label3.Text = "苗字は5文字以下までです。";
+                return;
+            }
+
+            if (F_name.Length > 6)
+            {
+                label3.Text = "名前は5文字以下までです。";
+                return;
+            }
+
+            label3.Text = L_name + " " + F_name;
+
+        }
     }
 }
