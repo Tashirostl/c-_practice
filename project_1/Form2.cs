e2d51bd7a6fd0445becbd13d199b72f4ee4df861
Tashirostl <stl.tashiro@gmail.com>
2016-12-03 23:46:26 +0900

    あ」

--- project_1/Form2.cs
+++ project_1/Form2.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace project_1
 {
     public partial class Form2 : Form
@@ -16,224 +17,94 @@ namespace project_1
         {
             InitializeComponent();
         }
-        float value1;                       //作業用変数
-        int OperatorButtonFlag;             //演算子判定用フラグ
-        static int ButtonPushFlag;                 //数字ボタン判定用フラグ
 
-        string LabelValue;
+        KeisankiButton keisan = new KeisankiButton();
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            if (ButtonPushFlag == 0) { LabelValue = ""; }
-            LabelValue += 1;
-            label1.Text = LabelValue;
-            ButtonPushFlag = 1;
+            keisan.NumberButton(1, ref label1);
         }
 
         private void Button2_Click(object sender, EventArgs e)
         {
-            if (ButtonPushFlag == 0) { LabelValue = ""; }
-            LabelValue += 2;
-            label1.Text = LabelValue;
-            ButtonPushFlag = 1;
+            keisan.NumberButton(2, ref label1);
         }
 
         private void Button3_Click(object sender, EventArgs e)
         {
-            if (ButtonPushFlag == 0) { LabelValue = ""; }
-            LabelValue += 3;
-            label1.Text = LabelValue;
-            ButtonPushFlag = 1;
+            keisan.NumberButton(3, ref label1);
         }
 
         private void Button4_Click(object sender, EventArgs e)
         {
-            if (ButtonPushFlag == 0) { LabelValue = ""; };
-            LabelValue += 4;
-            label1.Text = LabelValue;
-            ButtonPushFlag = 1;
+            keisan.NumberButton(4, ref label1);
         }
 
         private void Button5_Click(object sender, EventArgs e)
         {
-            if (ButtonPushFlag == 0) { LabelValue = ""; }
-            LabelValue += 5;
-            label1.Text = LabelValue;
-            ButtonPushFlag = 1;
+            keisan.NumberButton(5, ref label1);
         }
 
         private void Button6_Click(object sender, EventArgs e)
         {
-            if (ButtonPushFlag == 0) { LabelValue = ""; }
-            LabelValue += 6;
-            label1.Text = LabelValue;
-            ButtonPushFlag = 1;
+            keisan.NumberButton(6, ref label1);
         }
 
         private void Button7_Click(object sender, EventArgs e)
         {
-            if (ButtonPushFlag == 1) { LabelValue = ""; };
-            LabelValue += 7;
-            label1.Text = LabelValue;
-            ButtonPushFlag = 1;
+            keisan.NumberButton(7, ref label1);
         }
 
         private void Button8_Click(object sender, EventArgs e)
         {
-            if (ButtonPushFlag == 0) { LabelValue = ""; };
-            LabelValue += 8;
-            label1.Text = LabelValue;
-            ButtonPushFlag = 1;
+            keisan.NumberButton(8, ref label1);
         }
 
         private void Button9_Click(object sender, EventArgs e)
         {
-            if (ButtonPushFlag == 0) { LabelValue = ""; }
-            LabelValue += 9;
-            label1.Text = LabelValue;
-            ButtonPushFlag = 1;
+            keisan.NumberButton(9, ref label1);
         }
 
         private void Button0_Click(object sender, EventArgs e)
         {
-            if(LabelValue == "") { return; }
-            if (ButtonPushFlag == 0) { LabelValue = ""; }
-            LabelValue += 0;
-            label1.Text = LabelValue;
-            ButtonPushFlag = 1;
+            keisan.NumberButton(0, ref label1);
         }
 
         private void PointButton_Click(object sender, EventArgs e)
         {
-            if (ButtonPushFlag == 0) { LabelValue = ""; }
-            if (LabelValue == "") { LabelValue += 0; }
-            LabelValue += ".";
-            label1.Text = LabelValue;
-            ButtonPushFlag = 1;
+            keisan.NumberButton(10, ref label1);
         }
 
         private void SumButton_Click(object sender, EventArgs e)
         {
-            if (ButtonPushFlag == 0)                //数字ボタンを押さずに+ボタンを押した場合
-            {
-                //何もしない
-            }
-            else　                                  //数字ボタンを押してから+ボタンを押した場合
-            {
-                value1 += float.Parse(LabelValue); 
-                ButtonPushFlag = 0;
-                label1.Text = value1.ToString();
-            }
-
-            OperatorButtonFlag = 1;                 //演算子判定用フラグ（+ボタン)
+            keisan.ArithmeticOperatorButton(1,ref label1);
         }
         
         private void DifferenceButton_Click(object sender, EventArgs e)
         {
-            if (ButtonPushFlag == 0)                //数字ボタンを押さずに-ボタンを押した場合
-            {
-                //何もしない
-            }
-            else　                                  //数字ボタンを押してから-ボタンを押した場合
-            {
-                if (value1 == 0)
-                {
-                    value1 = float.Parse(LabelValue);
-                    ButtonPushFlag = 0;
-                    OperatorButtonFlag = 2;
-                    return;
-                }
-                value1 -= float.Parse(LabelValue);
-                ButtonPushFlag = 0;
-                label1.Text = value1.ToString();
-            }
-
-            OperatorButtonFlag = 2;                 //演算子判定用フラグ（-ボタン)
+            keisan.ArithmeticOperatorButton(2,ref label1);
         }
 
         private void ProductButton_Click(object sender, EventArgs e)
         {
-            if (ButtonPushFlag == 0)                //数字ボタンを押さずに*ボタンを押した場合
-            {
-                //何もしない
-            }
-            else　                                  //数字ボタンを押してから*ボタンを押した場合
-            {
-                if(value1 == 0)
-                {
-                    value1 = float.Parse(LabelValue);
-                    ButtonPushFlag = 0;
-                    OperatorButtonFlag = 3;
-                    return;
-                }
-                value1 *= float.Parse(LabelValue);
-                ButtonPushFlag = 0;
-                label1.Text = value1.ToString();
-            }
-
-            OperatorButtonFlag = 3;                 //演算子判定用フラグ（*ボタン)
+            keisan.ArithmeticOperatorButton(3,ref label1);
         }
 
 
         private void QuotientButton_Click(object sender, EventArgs e)
         {
-            if (ButtonPushFlag == 0)                //数字ボタンを押さずに/ボタンを押した場合
-            {
-                //何もしない
-            }
-            else　                                  //数字ボタンを押してから/ボタンを押した場合
-            {
-                if (value1 == 0)
-                {
-                    value1 = float.Parse(LabelValue);
-                    ButtonPushFlag = 0;
-                    OperatorButtonFlag = 4;
-                    return;
-                }
-                value1 /= float.Parse(LabelValue);
-                ButtonPushFlag = 0;
-                label1.Text = value1.ToString();
-            }
-
-            OperatorButtonFlag = 4;                 //演算子判定用フラグ（/ボタン)
+            keisan.ArithmeticOperatorButton(4, ref label1);
         }
 
         private void RESET_Click(object sender, EventArgs e)
         {
-            value1 = 0;
-            LabelValue = "";
-            label1.Text = LabelValue;
-            ButtonPushFlag = 0;
-            OperatorButtonFlag = 0;
+            keisan.ResetButton(ref label1);
         }
 
         private void EqualButton_Click(object sender, EventArgs e)
         {
-            switch (OperatorButtonFlag)
-            {
-                case 1:         //  +演算子
-                    value1 += float.Parse(LabelValue);
-                    label1.Text = value1.ToString();
-                    ButtonPushFlag = 0;
-                    break;
-                case 2:         //  -演算子
-                    value1 -= float.Parse(LabelValue);
-                    label1.Text = value1.ToString();
-                    ButtonPushFlag = 0;
-                    break;
-                case 3:         //  *演算子
-                    value1 *= float.Parse(LabelValue);
-                    label1.Text = value1.ToString();
-                    ButtonPushFlag = 0;
-                    break;
-                case 4:         //  /演算子
-                    value1 /= float.Parse(LabelValue);
-                    label1.Text = value1.ToString();
-                    ButtonPushFlag = 0;
-                    break;
-                default:
-                    break;
-            }
+            keisan.AnswerDisplay(ref label1);
         }
     }
 }
+public class KeisankiButton { }
---
+++ project_1/Keisanki.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project_1
+{
+    public class KeisankiButton
+    {
+        public int ButtonPushFlag { get; private set; }
+        public int OperatorButtonFlag { get; private set; }
+        public string LabelValue { get; private set; }
+        public float value1 { get; private set; }
+        
+        #region
+
+
+        /// <summary>
+        /// ラベルに表示します
+        /// </summary>
+        /// <param name="Display">labelを参照渡しで渡します。</param>
+        public void AnswerDisplay(ref Label Display)
+        {
+            switch(OperatorButtonFlag)
+            {
+                case 1:
+                    value1 += float.Parse(LabelValue);
+                    break;
+                case 2:
+                    value1 -= float.Parse(LabelValue);
+                    break;
+                case 3:
+                    value1 *= float.Parse(LabelValue);
+                    break;
+                case 4:
+                    value1 /= float.Parse(LabelValue);
+                    break;
+            }
+            ButtonPushFlag = 0;
+            Display.Text = value1.ToString();
+        }
+        /// <summary>
+        /// 数字ボタンの処理
+        /// </summary>
+        /// <param name="NumberId">押した番号のidを渡します。</param>
+        /// <param name="Display">labelを参照渡しで渡します。</param>
+        public void NumberButton(int NumberId,ref Label Display)
+        {
+            if (ButtonPushFlag == 0) { LabelValue = ""; }
+            switch (NumberId)
+            {
+                case 0:
+                    if (LabelValue == "") { return; }
+                    LabelValue += 0;
+                    break;
+
+                case 1:
+                    LabelValue += 1;
+                    break;
+
+                case 2:
+                    LabelValue += 2;
+                    break;
+
+                case 3:
+                    LabelValue += 3;
+                    break;
+
+                case 4:
+                    LabelValue += 4;
+                    break;
+
+                case 5:
+                    LabelValue += 5;
+                    break;
+
+                case 6:
+                    LabelValue += 6;
+                    break;
+
+                case 7:
+                    LabelValue += 7;
+                    break;
+
+                case 8:
+                    LabelValue += 8;
+                    break;
+
+                case 9:
+                    LabelValue += 9;
+                    break;
+
+                case 10:
+                    if (ButtonPushFlag == 0) { LabelValue = ""; }
+                    if (LabelValue == "") { LabelValue += 0; }
+                    LabelValue += ".";
+                    break;
+
+                default:
+                    break;
+            }
+            Display.Text = LabelValue;
+            ButtonPushFlag = 1;
+        }
+
+
+        /// <summary>
+        /// 演算子のボタンの処理
+        /// </summary>
+        /// <param name="OperaterId">どの演算子を押したか判定するためのidを渡します。</param>
+        /// <param name="Display">labelを参照渡しで渡します。</param>
+        public void ArithmeticOperatorButton(int OperaterId,ref Label Display)
+        {
+            OperatorButtonFlag = OperaterId;                
+
+            if (ButtonPushFlag == 0){ }
+            else　                                  
+            {
+                if (value1 == 0)
+                {
+                    value1 = float.Parse(LabelValue);
+                    ButtonPushFlag = 0;
+                    return;
+                }
+                AnswerDisplay(ref Display);
+            }
+        }
+
+        /// <summary>
+        /// リセットボタンの処理
+        /// </summary>
+        /// <param name="Display">labelを参照渡しで渡します。</param>
+        public void ResetButton(ref Label Display)
+        {
+            value1 = 0;
+            LabelValue = "";
+            Display.Text = LabelValue;
+            ButtonPushFlag = 0;
+            OperatorButtonFlag = 0;
+        }
+
+        
+        #endregion
+    }
+}
